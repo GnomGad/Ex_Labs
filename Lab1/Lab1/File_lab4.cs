ad20a2c5398cd39c0089d5ed2de83d2d25869338
Евгений Лазаренко <zhenya.lazorenko@mail.ru>
2018-12-08 19:51:04 +0300

    юпдейт

--- Lab1/Lab1/File_lab4.cs
+++ Lab1/Lab1/File_lab4.cs
@@ -7,7 +7,7 @@ namespace Labs
 {
     class lab4
     {
-        public static void Ex_1()// не верно робит, над починить
+        public static void Ex_1()// починил
         {
             int count = 0;
             
@@ -15,48 +15,73 @@ namespace Labs
             
             //StringBuilder str_b = new StringBuilder();
 
-             //str_s = "шла саша по шоссе и сосала сушку.";
+             //str_s = "шла Cаша по шоссе и сосала сушку.";
             //str_s = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ"
 
-            Console.WriteLine("Первый способ");
-            for(int i =0;i<str_s.Length;i++)// i юзается как тестируемый символ
+            Console.WriteLine("Первый способ");// Обработка строки через массивы // суть примерно похоже на заполнение массива
+            for (int i =0;i<str_s.Length;i++)// i юзается как тестируемый символ
             {
-                for (int j = 0; j < str_s.Length; j++)//j это  всех символы
+                for (int j = 0; j < str_s.Length; j++)//j это  всех символы  
                     if (i != j && str_s[i] == str_s[j]) count++;// наши требования - i !=j
                 if(count==0)Console.Write(str_s[i]);
                 count = 0;
             }
-            int n = 0;
-            Console.WriteLine("\nВторой способ");// чинить-------------------------------------------------
-            for(int i = 0;i<str_s.Length;i++)// главный символ
+
+            Console.WriteLine("\nВторой способ");
+            for (int i = 0; i< str_s.Length;i++)// проверка каждого символа
             {
-                for (int j = 0; n != -1||n<str_s.Length;j++)// здесь количество повторо будет
-                {
-                    if (i != str_s.IndexOf(str_s[i], n)&& str_s.IndexOf(str_s[i],n)==str_s[j])
-                        count++;
-                    n++;
-                }
-                if (count == 0) Console.Write(str_s[i]);
-                count = 0;
                 
+               for(int j =0;str_s.IndexOf(str_s[i],j)!=-1;)// проверка вхождений как только вхождение не будет замечено, то индекс оф выведет -1
+                {
+                    if (str_s[str_s.IndexOf(str_s[i], j)] == str_s[i])
+                    {// идет проверка, если элемент с индексом индекс оф будет равен нашему элементу i, то мы заходим и делаем счетчик
+                        count++;
+                        j = str_s.IndexOf(str_s[i], j) + 1;// это шо бы у нас в поиск уже не попадались элементы которые я нашел, и по этому +1
+                    }
+                }
+                if (count == 1) Console.Write(str_s[i]);// при одинарном символе, индекс оф может только 1 раз закоунтиться
+                count = 0;
+
             }
+            
+            Console.WriteLine();
 
         }
-        public static void Ex_2()//херня
+        public static void Ex_2()//доделать
         {
-            string skob = "()";
+           
             string str_s = Console.ReadLine();
+            int sleep = 0;// это счетчик пропусков, пока он не ноль, мы будем пропускать вывода номера слова при особых символах
             Console.WriteLine("Первый способ");
-            int n = -2;
-            for (int i = 0, count = 1; n != -1; i = n + 1, count++ )
+
+            for (int i = 0,count =1; i < str_s.Length; i++)//обработка основной каждого элемента
             {
-                n = str_s.IndexOf(' ',i);
-                if (n == -1) break;
-                for(int j = 0;j<3;j++)
-                str_s = str_s.Insert(n, "(" + count + ")");
-                Console.WriteLine(n);
+                if ((str_s[i] == ' ' || str_s[i] == ',' || str_s[i] == '.' || str_s[i] == '-')&&sleep==0)// проверка на нужный символ, а так же слип
+                {
+                    Console.Write("({0})", count);// если уж зашли,то должны вывести число слова
+                    Console.Write(str_s[i]); //ну и собственно символ
+                    count++;// это счетчик слов
+                    for(; ;sleep++)// божественный слип не делал условия выхода, что бы сделать потом
+                    {
+                        if (i + sleep < str_s.Length)// проверка, что бы не улетать за границы массива
+                        {//собственно, если после конца слова будут идти еще такие символы, то мы их должны посчитать
+                            // что бы потом, их даже не использовать
+                            if (str_s[i + sleep] == ' ' || str_s[i + sleep] == ',' || str_s[i + sleep] == '.' || str_s[i + sleep] == '-');
+                            else break;
+                        }
+                        else break;// выход если улетели
+                    }
+                }
+                else Console.Write(str_s[i]);// если обычный символ то выводим
+                if (sleep != 0) sleep--;// на каждой итерации будет идти -- и так до нуля, что бы не запороть слова
             }
-            Console.WriteLine(str_s);
+            Console.WriteLine();
+            Console.WriteLine("Второй способ");
+            for (int i = 0; i < str_s.Length; i++)//
+            {
+
+            }
+
         }
 
     }
