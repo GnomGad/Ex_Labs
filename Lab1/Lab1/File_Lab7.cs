153f05b6fcd7c0d69ee2f89c15985fad083ed16f
GnomGad <zhenya.lazorenko@mail.ru>
2019-03-10 16:41:13 +0300

    Добавил выполнил 1 задание

--- Lab1/Lab1/File_Lab7.cs
+++ Lab1/Lab1/File_Lab7.cs
@@ -7,34 +7,43 @@ namespace Labs
 {
     class FileLabLibrary7 
     {
-        void Swap(ref int a, ref int b)
+        
+
+        public void InsertionSort(ref GetTvshow[] Tv, int l, int r)
         {
-            int tmp = a;
-            a = b;
-            b = tmp;
-        }
-        public void SelectionSort(int[] a, int l,int r)
-        {
-            for (int i = l; i < r; i++)
-            {
-                int min = i;
-                for (int j = i + 1; j <= r; j++)
-                {
-                    if (a[j] < a[min])
+            for (int i = l + 1; i <= r; i++)
+                for (int j = i; j > l; j--)
+                    if ((int)Tv[j - 1].Value > (int)Tv[j].Value)
                     {
-                        min = j;
+                        // Swap((int)Tv[j - 1].Value, (int)Tv[j].Value, j);
+                        //присвоение tmp элементы j-1
+                        int tmpValue =(int) Tv[j - 1].Value;
+                        string tmpNameTv = Tv[j - 1].NameTV;
+                        string tmpNameArtist = Tv[j - 1].NameArtist;
+                        char tmpType = Tv[j - 1].Type;
+                        int tmpN = Tv[j - 1].n;
+
+                        //присвоение j-1 элементы из j
+                        Tv[j - 1].Value = Tv[j].Value;
+                        Tv[j - 1].NameTV = Tv[j].NameTV;
+                        Tv[j - 1].NameArtist = Tv[j].NameArtist;
+                        Tv[j - 1].Type = Tv[j].Type;
+                        Tv[j - 1].n = Tv[j].n;
+
+                        //присвоение j из tmp
+                        Tv[j].check_for_value(tmpValue.ToString(), out string er);
+                        Tv[j].check_for_tyoe(tmpType, out string er1);
+                        Tv[j].NameArtist = tmpNameArtist;
+                        Tv[j].NameTV = tmpNameTv;
+                        Tv[j].n = tmpN;
                     }
-                }
-                Swap(ref a[i], ref a[min]);
-            }
-            foreach (int k in a)
-                Console.WriteLine(k);
         }
 
-       
 
 
-       
+
+
+
     }
     class Lab7Manager
     {
@@ -50,9 +59,13 @@ namespace Labs
     {
        static public void Begin()
         {
-            FileLabLibrary7 NewLib7 = new FileLabLibrary7();
-            
-            Ex_1();
+            File_lab6.ReadAndSetTvShowsFilePlaylist();// инициализация TV_Shows[]
+            int i = 0;
+            for (i = 0; i < MaxStrinTable && Tv_Shows[i].NameTV != null; i++);// узнаем сколько не пустых элементов
+            FileLabLibrary7 fl7 = new FileLabLibrary7();
+            fl7.InsertionSort(ref Tv_Shows, 0, i-1);
+            File_lab6.main();
+
         }
     }
 
