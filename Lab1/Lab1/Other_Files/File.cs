4dec01c5acbfbb3a6455e5294b2514f3ad05f2a2
Zhenya <zhenya.lazorenko@mail.ru>
2019-04-09 13:07:33 +0300

    Add new file from mobile

---
+++ Lab1/Lab1/Other_Files/File.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public static class Program 
+    {
+        
+        public static void Main() 
+        {
+           string kek = "((5+3)(5+14+24))";
+           Test test = new Test(kek);
+        }
+    }
+    
+    public class Test
+    {
+        Stack<char> testStack = new Stack<char>();
+        
+        public string Text {get;set;}
+        
+        public Test(string text)
+        {
+            Text = text;
+            Fu();
+            
+        }
+        
+        public void Fu()
+        {
+            for(int i = 0;i<Text.Length;i++)
+            {
+                if(Text[i] == '(')
+                {
+                    testStack.Push(Text[i]);
+                }
+                else if(Text[i] == ')')
+                {    //если количество элементтв нуль то выдать ошибку
+                    if(testStack.Count != 0 && testStack.Peek() == '(')
+                    {
+                        testStack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выражение неверное");
+                        return;
+                    }
+                }
+                
+            }
+            if(testStack.Count != 0)
+            Console.WriteLine("Выражение неверное");
+            else
+                Console.WriteLine("Выражение верное");
+            
+        }
+        
+    }
+}
